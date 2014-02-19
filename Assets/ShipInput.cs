16c00faf0513390bd800acfe11d642ead3c1624b
PyroDragn <ericounderdown@gmail.com>
2014-02-19 21:38:49 +0000

    Added Temp Model
    Added temporary model for galleon. Included textured grid for ship and
    visible movement during testing. Galleon model is oriented in wrong
    direction so movement translation has been changed from Vector3.forward
    to Vector3.left until appropriate ship model is commissioned.

--- Assets/ShipInput.cs
+++ Assets/ShipInput.cs
@@ -19,7 +19,7 @@ public class ShipInput : MonoBehaviour {
 		//transform.Rotate(new Vector3(0, 0,  Time.deltaTime * -3f * (Mathf.PI * Input.GetAxis("Rudder"))));
 		inertia = Mathf.Lerp(inertia, Input.GetAxis("Sails"), Time.deltaTime);
 		transform.Rotate(Vector3.up * Input.GetAxis("Rudder"));
-		transform.Translate(Vector3.forward * inertia * Time.deltaTime);
+		transform.Translate(Vector3.left * (inertia * 10) * Time.deltaTime);
 		if (Input.GetButton("Menu")){
 			mainCameraObj.SetActive(true);
 			GameObject.Find ("galleon").SetActive(false);
