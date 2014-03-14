00d46cf3e39aef5a31864ae3cc2b27552f7eee72
Searonik <eric_war02@yahoo.com>
2014-03-14 09:41:38 -0400

    Altered Ship movement to move the world. Ship still rotates, world does not. May still make world rotate instead, too.
    Signed-off-by: Searonik <eric_war02@yahoo.com>

--- Assets/ShipInput.cs
+++ Assets/ShipInput.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ShipInput : MonoBehaviour {
 
-	float inertia;
+
 	public GameObject mainCameraObj;
 
 	void awake(){
@@ -11,15 +11,13 @@ public class ShipInput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		inertia = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//transform.Rotate(new Vector3(0, 0,  Time.deltaTime * -3f * (Mathf.PI * Input.GetAxis("Rudder"))));
-		inertia = Mathf.Lerp(inertia, Input.GetAxis("Sails"), Time.deltaTime);
-		transform.Rotate(Vector3.up * Input.GetAxis("Rudder"));
-		transform.Translate(Vector3.left * (inertia * 10) * Time.deltaTime);
+		transform.Rotate(Vector3.up * Input.GetAxis("Rudder") * .2f);
 		if (Input.GetButton("Menu")){
 			mainCameraObj.SetActive(true);
 			GameObject.Find ("galleon").SetActive(false);
