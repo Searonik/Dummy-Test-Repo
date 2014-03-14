2c5801bfbf330484a09e3273ea3108d2e3900b9c
Searonik <eric_war02@yahoo.com>
2014-03-14 09:43:25 -0400

    Altered Ship movement to move the world. Ship still rotates, world does not. May still make world rotate instead, too.
    Signed-off-by: Searonik <eric_war02@yahoo.com>

---
+++ Assets/WorldManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+	float inertia;
+	public GameObject ship;
+
+	// Use this for initialization
+	void Start () {
+		inertia = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		inertia = Mathf.Lerp(inertia, Input.GetAxis("Sails"), Time.deltaTime);
+		transform.Translate(ship.transform.right * (inertia * 10) * Time.deltaTime);
+
+
+	}
+}
