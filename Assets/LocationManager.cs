fe012a4f76a0c082dad8e3b4243c7efd130ea751
Searonik <eric_war02@yahoo.com>
2014-03-14 10:28:02 -0400

    Added prototype functionality for making world parts "inactive". Merely disables the Rendered and Collider, allowing script to still run.
    Signed-off-by: Searonik <eric_war02@yahoo.com>

---
+++ Assets/LocationManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocationManager : MonoBehaviour {
+
+	public GameObject ship;
+	bool Visible = true;
+
+	// Use this for initialization
+	void Start (){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (ship.activeSelf){
+			if (Visible && BeyondDistance(ship)){
+				Visible = false;
+				this.renderer.enabled = false;
+				this.collider.enabled = false;
+			}
+			else if (!Visible && !BeyondDistance(ship)){
+				Visible = true;
+				this.renderer.enabled = true;
+				this.collider.enabled = true;
+			}
+		}
+
+	}
+
+	private bool BeyondDistance(GameObject thing){
+
+		if ((Mathf.Pow((thing.transform.position.x - this.transform.position.x), 2f) + Mathf.Pow((thing.transform.position.z - this.transform.position.z), 2f)) > 250000){
+				return true;
+		}
+		else{
+				return false;
+		}
+	}
+}
